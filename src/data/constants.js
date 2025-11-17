17eba02efe7e6885cca4e3fa96968231a1ffefbb
ryna6 <ryanjw629.h@gmail.com>
2025-11-17 03:19:33 -0500

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,14 +1,21 @@
 // constants.js
 export const TIMEFRAMES = {
   ONE_DAY: '1D',
-  ONE_WEEK: '1W'
+  ONE_WEEK: '1W',
 };
 
 export const STORAGE_KEYS = {
+  // Legacy global timeframe key (no longer used for new code)
   timeframe: 'md_timeframe',
   sp500Cache: 'md_sp500_cache',
   sectorCache: 'md_sector_cache',
   cryptoCache: 'md_crypto_cache',
   earningsCache: 'md_earnings_cache',
-  companyProfilesCache: 'companyProfilesCache'
+  companyProfilesCache: 'companyProfilesCache',
+};
+
+export const TIMEFRAME_STORAGE_KEYS = {
+  sp500: 'md_sp500_timeframe',
+  sectors: 'md_sectors_timeframe',
+  crypto: 'md_crypto_timeframe',
 };
